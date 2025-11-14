a376f7e108307812dd95c53be074ca58a7848830
kdawid <karol.dawid27@gmail.com>
2025-11-14 15:44:07 +0100

    start

---
+++ sportsfacilitysystem/src/main/java/mckd/sportsfacilitysystem/SportsfacilitysystemApplication.java
@@ -0,0 +1,13 @@
+package mckd.sportsfacilitysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsfacilitysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsfacilitysystemApplication.class, args);
+	}
+
+}
--- sportsfacilitysystem/src/test/java/mckd/sportsfacilitysystem/SportsfacilitysystemApplicationTests.java
+++ sportsfacilitysystem/src/test/java/mckd/sportsfacilitysystem/SportsfacilitysystemApplicationTests.java
@@ -1,13 +1,13 @@
-package pas.restapp;
+package mckd.sportsfacilitysystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestAppApplicationTests {
+class SportsfacilitysystemApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
